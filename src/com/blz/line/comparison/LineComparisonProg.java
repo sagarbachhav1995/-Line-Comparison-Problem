e83c6df14975de4f0d0f855143b1f4f9358adbcb
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-09 22:22:32 -0700

    Added : Line Comparison Program

---
+++ src/com/blz/line/comparison/LineComparisonProg.java
@@ -0,0 +1,10 @@
+package com.blz.line.comparison;
+
+public class LineComparisonProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
